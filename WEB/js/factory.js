8ee87cdac66f533da5ebed9d4073d3c24f32c1ce
Eloi Carracedo <eloimuns@gmail.com>
2017-11-25 16:01:27 +0100

    minor changes

--- WEB/js/factory.js
+++ WEB/js/factory.js
@@ -2,11 +2,13 @@ app.factory('Service', function ($http) {
   var url = 'http://localhost:8888'
   return {
     addTrack: function (track) {
-      console.log(track)
       return $http.post(url + '/addTruck', track);
     },
     getTracks: function(){
         return $http.get(url + '/trucks');
+    },
+    getTrack: function(id){
+      return $http.get(url + '/truck/' + id);
     }
   }
 });
--- WEB/js/trackdetail.js
+++ WEB/js/trackdetail.js
@@ -1,6 +1,22 @@
-app.controller('TrackDetailCtrl', function($scope, $location, $rootScope, Service) {
-
+app.controller('TrackDetailCtrl', function($scope, $location, $routeParams, Service) {
+  $scope.sensorData = {};
+  $scope.showPasteArea = false;
+  Service.getTrack($routeParams.id).then(function(success) {
+    if (success){
+      if (success.data){
+        $scope.track = success.data;
+      }
+    }
+  })
   $scope.back = function(track) {
     $location.path('/');
+  }
+
+  $scope.tooglePasteArea = function(){
+    $scope.showPasteArea = !$scope.showPasteArea;
+  }
+
+  $scope.save = function(){
+    console.log($scope.sensorData)
   }
 });
